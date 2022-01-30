efb6df29f0c0067b06ada98548fee08a742972f9
Nitin Misra <nitin.misra@gojek.com>
2022-01-30 21:08:04 +0530

    Elixir pacman-rules: Implement eat_ghost? function

--- elixir/pacman-rules/lib/rules.ex
+++ elixir/pacman-rules/lib/rules.ex
@@ -1,7 +1,14 @@
 defmodule Rules do
-  def eat_ghost?(power_pellet_active, touching_ghost) do
-    # Please implement the eat_ghost?/2 function
+  @doc """
+  Returns true if power_pellet is active and is touching a ghost
+  """
+  @spec eat_ghost?(Boolean.t(), Boolean.t()) :: Boolean.t()
+  def eat_ghost?(true = _power_pellet_active, true = _touching_ghost) do
+    true
   end
+
+  def eat_ghost?(_power_pellet_active, _touching_ghost),
+    do: false
 
   def score?(touching_power_pellet, touching_dot) do
     # Please implement the score?/2 function
